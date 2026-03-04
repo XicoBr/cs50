2b67fc233134761d23b0b037ee526cd6def5ea73
XicoBr <diego.fong@hotmail.com>
2026-03-04 19:24:47 -0300

    atualizando arquivos da pasta l5

--- l5.data_structures/list1.c
+++ l5.data_structures/list1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct node
 {
     int number;
     struct node *next;
 } node;
+
 
 int main(void)
 {
@@ -18,20 +20,11 @@ int main(void)
         {
             return 1;
         }
-        printf("%ist number: ", i + 1);
+        printf("Number: ");
         scanf("%i", &n->number);
+        
         n->next = list;
-    
         list = n;
     }
 
-    node *ptr = list;
-
-    while(ptr != NULL)
-    {
-        printf("%i\n", ptr->number);
-        ptr = ptr->next;
-    }
-    // linha de teste
-    return 0;
 }
