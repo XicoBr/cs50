627ff7caad6c689e300dc3d45b5632b5c59a4efb
XicoBr <diego.fong@hotmail.com>
2026-03-09 08:09:35 -0300

    atualizando lista encadeada

--- l5.data_structures/list1.c
+++ l5.data_structures/list1.c
@@ -31,14 +31,29 @@ int main(void)
         {
             list = n;
         }
+        // se o campo number de n for menor que o number de list, faz um preppend (adiciona no inicio)
+        else if (n->number < list->number)
+        {
+            n->next = list;
+            list = n;
+        }
         else 
         {
+            // percorre os nodes na lista
             for (node *ptr = list; ptr != NULL; ptr = ptr->next)
             {
                 // se ptr chegar no final da lista
                 if (ptr->next == NULL)
                 {
-                    // ptr aponta para onde n está apontando
+                    // ptr aponta para onde n está apontando == faz um append - adição no fim da lista
+                    ptr->next = n;
+                    break;
+                }
+                
+                // se no meio da lista
+                if (n->number < ptr->next->number)
+                {
+                    n->next = ptr->next;
                     ptr->next = n;
                     break;
                 }
